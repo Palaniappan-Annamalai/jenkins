f91cc655e7042254e05d45d176631aeed2f1a86f
IyyappanAbly <aiyyappan38@gmail.com>
2023-11-16 13:57:51 +0530

    second commit

---
+++ src/main/java/com/cs/jenkinsdemo/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.cs.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping("/jenkins")
+    public String getData(){
+        return "Hello from Jenkins";
+    }
+
+
+}
